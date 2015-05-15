eff18de46caf656c21ef8d4b79c0b8b3ed4611e3
git1975 <denzorin@yahoo.com>
2015-05-15 16:36:26 +0400

    target remove

--- com.igo.server/target/work/resources/com/igo/server/ProcessInstanceFactory.groovy
+++ com.igo.server/target/work/resources/com/igo/server/ProcessInstanceFactory.groovy
@@ -2,15 +2,15 @@ package com.igo.server
 
 class ProcessInstanceFactory {
 	static def createInstance(long idprocess){
-		print "createInstance..."
+		print "ProcessInstanceFactory.createInstance...idprocess=" + idprocess
 		
 		List process = Process.findAll("from Process as p where p.id = ?", [idprocess])
-		//for(Process item : process){
-		process.each{item -> 
-			Task[] tasks = item.getTasks()
+		for(Process item : process){
+		//process.each{item -> 
+			Task[] tasks = item.tasks
 			print tasks.toString()
 			tasks.each{t -> 
-				Queue msg = new Queue(type: 'Task', finished: false, description: t.getDescription(), idprocess: idprocess)
+				Queue msg = new Queue(type: 'Task', finished: false, description: t.description, idprocess: idprocess, ord: t.ord)
 				.save(failOnError: true)
 			}
 		}
