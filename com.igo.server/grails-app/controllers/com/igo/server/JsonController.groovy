5d9c72c4cf2f732c781685fd4d7e721291bcba33
git1975 <denzorin@yahoo.com>
2015-06-30 17:16:22 +0300

    1

--- com.igo.server/grails-app/controllers/com/igo/server/JsonController.groovy
+++ com.igo.server/grails-app/controllers/com/igo/server/JsonController.groovy
@@ -1,8 +1,6 @@
 package com.igo.server
 
 import grails.converters.JSON
-import java.util.regex.Matcher
-import java.util.regex.Pattern;
 
 class JsonController {
 	def commandService
@@ -22,58 +20,7 @@ class JsonController {
 		String lastHash = params.hash
 		log.debug("JsonController.show.hash=" + params.hash)
 
-		Date now = new Date()
-		Date processDate = new Date()
-
-		//Определим время текущей задачи, для демо
-		List<Queue> listCurrent = Queue.findAll("from Queue as q where q.type = 'Task' and ord = 1 order by startdate desc", [], [max: 1])
-		if(listCurrent != null && listCurrent.size() > 0){
-			Queue q = listCurrent.get(0)
-			processDate = q.startdate
-		}
-
-		//Показать зеленым цветом инф. сообщение в любом случае, если текущее время между startdate и signaldate
-		//Показать желтым цветом сообщение с запросом, если время текущее между signaldate и enddate
-		//Показать красным цветом сообщение, если время текущее больше enddate и статус = TIMEOUT (т.е. пользователь не реагирует на запросы и система перевела статус в TIMEOUT)
-		List<Queue> listGreen = Queue.findAll("from Queue as q where q.type = 'Task' and startdate < ? and signaldate > ? and startdate >= ?", [now, now, processDate])
-		List<Queue> listYellow = Queue.findAll("from Queue as q where q.type = 'Task' and signaldate < ? and enddate > ? and startdate >= ?", [now, now, processDate])
-		List<Queue> listRed = Queue.findAll("from Queue as q where q.type = 'Task' and enddate < ? and q.status = 'INIT' and startdate >= ? and ord != 1 and ord != 4", [now, processDate])
-
-		//println "Green-" + listGreen.size + "-Yellow-" + listYellow.size
-
-		def messages = new ArrayList();
-
-		//Покажем инф. сообщение о начале этапа
-		for(Queue q: listGreen){
-			def mes = getMessage(q, true)
-			if(mes == null){
-				mes = new MessageCommand()
-				mes.type = "INFO"
-			}
-
-			messages.add(mes)
-		}
-		//Покажем основные сообщения между signaldate и enddate
-		for(Queue q: listYellow){
-			def mes = getMessage(q, false)
-			if(mes != null){
-				messages.add(mes)
-			}
-		}
-		//Покажем сообщение о просрочке
-		for(Queue q: listRed){
-			def mes = getMessage(q, true)
-			if(mes == null){
-				mes = new MessageCommand()
-				mes.type = "INFO"
-			}
-			mes.setQueue(q)
-			mes.body = "Просрочка"
-			mes.type = "INFO"
-			mes.status = "INIT"
-			mes.color = 3
-			messages.add(mes)
-		}
+		def messages = commandService.getMessages();
 
 		//Check last hash
 		String hash = "";
@@ -97,42 +44,6 @@ class JsonController {
 		render messages as JSON;
 	}
 
-	def getMessage(Queue q, boolean isInfoStage){
-		Task t = Task.find("from Task as a where a.id = ?", [q.task.id])
-		Taskstatus ts
-		if(isInfoStage){
-			ts = Taskstatus.find("from Taskstatus as a where a.task=? and a.status=? and a.msgtype='INFO' and a.status='INIT'", [t, q.status])
-		} else {
-			ts = Taskstatus.find("from Taskstatus as a where a.task=? and a.status=? and ((a.msgtype!='INFO' and a.status='INIT')or(a.status!='INIT'))", [t, q.status])
-		}
-		//найден статус задачи, занесенный в шаблон
-		if(ts != null){
-			def mes = new MessageCommand()
-			mes.setQueue(q)
-			mes.body = ts.msgtext
-			mes.type = ts.msgtype
-			mes.status = q.status
-			mes.color = ts.color
-			mes.buttons = new Button[ts.buttons.size()]
-			for(int i = 0; i < ts.buttons.size(); i++){
-				mes.buttons[i] = ts.buttons[i]
-			}
-
-			mes.body = getMessageBody(q, mes.body)
-
-			//Check lifetime
-			if(mes.type == "CMD"){
-				long diff = (new Date()).getTime() - q.signaldate.getTime()
-				long diffMinutes = Math.round(diff/1000)
-				if(diffMinutes >= ts.lifetime*60){
-					mes.color = 3
-				}
-			}
-			return mes
-		}
-		return null
-	}
-	
 	def report() {
 		String lastHash = params.hash
 		log.debug("JsonController.report")
@@ -169,39 +80,6 @@ class JsonController {
 		}
 
 		render messages as JSON;
-	}
-
-	/**
-	 * @param  Queue q
-	 * @param String body
-	 * @return
-	 */
-	def getMessageBody(Queue q, String body){
-		Pattern pattern = Pattern.compile('(<.*?>)|(.+?(?=<|$))');
-		Matcher matcher = pattern.matcher(body);
-		def result = ''
-		while (matcher.find()) {
-			def s = matcher.group()
-			if('<process>'.equals(s)){
-				Process process = Process.find("from Process as a where a.id = ?", [q.idprocess])
-				s = process.description
-			} else if('<stage>'.equals(s)){
-				s = q.description
-			} else if(s.indexOf('<user=') >= 0){
-				int ix = s.indexOf('=');
-				def login = s.substring(ix + 1, s.length() - 1)
-				User user = User.find("from User as a where a.login = ?", [login])
-				if(user != null){
-					s = user.username
-				} else {
-					s = "?"
-				}
-			}
-
-			result += s
-		}
-
-		return result
 	}
 
 	def login() {
